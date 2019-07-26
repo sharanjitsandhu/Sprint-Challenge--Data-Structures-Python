9423f3800376a3de7e8bbd7af8254f6fd0d79a35
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-07-26 11:48:22 -0700

    Refactored name program

--- names/names.py
+++ names/names.py
@@ -1,3 +1,5 @@
+# ---------------------Original Solution---------------------#
+'''
 import time
 
 start_time = time.time()
@@ -17,6 +19,34 @@ for name_1 in names_1:
             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+'''
 
+# ---------------------My Solution---------------------#
+import time
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+# create a dictionary of names_2
+# where 'name' are keys and 'True' is a value
+dict_name_2 = dict((name, True) for name in names_2)
+
+duplicates = []
+for name_1 in names_1:
+    if name_1 in dict_name_2:
+        duplicates.append(name_1)
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+# Runtime: less than 0.01 seconds
