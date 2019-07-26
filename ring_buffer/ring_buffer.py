c5fde556de2f79469defbdc5b113100bdf263b70
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-07-26 10:25:33 -0700

    Finished Ring Buffer program

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,18 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+# append method adds elements to the buffer.
+    def append(self, item):
+        # add to storage based on current (index)
+        self.storage[self.current] = item
+        if self.current == self.capacity - 1:
+            self.current = 0
+        else:
+            self.current += 1
 
-  def get(self):
-    pass
+# get method returns all of the elements in the buffer in a list in their given order.
+    def get(self):
+        return [item for item in self.storage if item != None]
